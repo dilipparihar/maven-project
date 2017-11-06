ace98a0249cce494816bb2414d2ca7618d78de88
Dilip Parihar <Dilip.Parihar@das.ohio.gov>
2017-11-06 16:56:45 -0500

    add property

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -23,4 +24,11 @@ public class HelloMavenTest extends FunctionalTestCase {
         return "maven-project.xml";
     }
 
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    	System.out.println("\nDynamic Port Test Case 2: "+myPort.getNumber());
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }
 }
